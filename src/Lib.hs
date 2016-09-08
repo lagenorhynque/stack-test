2e9c2386cc1e7d5d41dd9280e27894ce31b685a1
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-09-08 19:50:34 +0900

    add a library dependency

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+import qualified Data.Map as M
+
+someMap :: M.Map String String
+someMap = M.fromList [("stack exec", "Hello, world!")]
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = print $ M.lookup "stack exec" someMap
